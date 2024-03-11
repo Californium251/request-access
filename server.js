8d21a0857bb6af4fdf0f4e40db78592c7e456331
Maxim Kurepov <californium251@gmail.com>
2024-03-11 11:26:06 +0400

    cors

--- server.js
+++ server.js
@@ -2,25 +2,24 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     optionsSuccessStatus: 200,
 };
 app.use(express.json());
 app.use(cors(corsOptions));
 app.get('/', function (req, res) {
     console.log('here');
-    res.send('ok');
+    res.send('Only post here');
 });
 app.post('/', function (req, res) {
     var createAirtableEntry = require('./index').createAirtableEntry;
     try {
         createAirtableEntry(req.body.data);
-        res.send('ok');
     }
     catch (e) {
         res.send(e);
     }
 });
-app.listen(3001, '0.0.0.0', function () {
-    console.log('Server listening on port 3000');
+app.listen(3007, '0.0.0.0', function () {
+    console.log('Server listening on port 3007');
 });
